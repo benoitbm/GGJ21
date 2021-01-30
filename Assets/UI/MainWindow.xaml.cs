97d67675814bad2777e6551e4d12a9b85b0257b9
Benoit <benoitbm@hotmail.fr>
2021-01-29 23:44:17 -0500

    Moving Noesis again
    As everytime you would enter in Play mode, the pop up would appear, thanks Obama.

---
+++ Assets/UI/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+   using Noesis;
+#else
+using System;
+using System.Windows;
+using System.Windows.Controls;
+#endif
+
+namespace GGJ21_UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : UserControl
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+#if NOESIS
+        private void InitializeComponent()
+        {
+            Noesis.GUI.LoadComponent(this, "Assets/UI/MainMenu.xaml");
+            Noesis.GUI.LoadXaml("Assets/UI/Test.xaml");
+            Noesis.GUI.LoadXaml("Assets/UI/UserControl1.xaml");
+        }
+#endif
+    }
+}
