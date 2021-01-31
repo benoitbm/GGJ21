6f94eb6baaf3b421ebc1da3e17970aecc23ef234
D_Kog <dani.kogandk@gmail.com>
2021-01-31 10:46:55 -0500

    Added Ground + level border

--- Assets/Code/Audio/MasterSound.cs
+++ Assets/Code/Audio/MasterSound.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class MasterSound : MonoBehaviour
 {
+   protected static MasterSound instance;
+    public static MasterSound Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType(typeof(MasterSound)) as MasterSound;
+
+                if (instance == null)
+                {
+                    Debug.LogWarning("There is no singleton of type " + typeof(MasterSound).ToString() +
+                                       " in the scene! Please ensure there's always one before playing.");
+
+                    Debug.LogWarning("Creating provisory " + typeof(MasterSound).ToString() + "...");
+                    instance = new GameObject(typeof(MasterSound).ToString()).AddComponent<MasterSound>();
+                }
+            }
+
+            return instance;
+        }
+        private set { instance = value; }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +34,19 @@ public class MasterSound : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void PlaySound(string eventname, GameObject passedObject)
     {
-        
+        AkSoundEngine.PostEvent(eventname, passedObject);
     }
+    // Update is called once per frame
+    public void PlaySound(string eventname)
+    {
+        AkSoundEngine.PostEvent(eventname, gameObject);
+    }
+    public void SetRTPC(string rtpcName, float value)
+    {
+        AkSoundEngine.SetRTPCValue(rtpcName, value);
+    }
+
+}
 }
