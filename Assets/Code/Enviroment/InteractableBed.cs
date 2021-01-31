8a93e5ffd7f0e6e8351cd530df79d5fe69113a98
Benoit <benoitbm@hotmail.fr>
2021-01-31 11:46:55 -0500

    Minimap render submit

--- Assets/Code/Enviroment/InteractableBed.cs
+++ Assets/Code/Enviroment/InteractableBed.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class InteractableBed : IInteractableObject
 {
     [SerializeField, Tooltip("Bool about if the tooth was already gained, showed in editor for testing purposes")]
-    bool m_ToothCollected = false;   
+    bool m_ToothCollected = false;
+
+    [SerializeField]
+    SpriteRenderer m_MinimapCheck;
+
+    public void Awake()
+    {
+        m_MinimapCheck.enabled = false;
+    }
 
     public override bool CanBeInteracted()
     {
@@ -19,6 +27,7 @@ public class InteractableBed : IInteractableObject
             player.IncreaseScore();
             player.RemoveMoney();
             m_ToothCollected = true;
+            m_MinimapCheck.enabled = true;
         }
         else
         {
--- Assets/Code/Enviroment/SpriteRandomaizer.cs
+++ Assets/Code/Enviroment/SpriteRandomaizer.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpriteRandomaizer : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    private SpriteRenderer m_MinimapBG;
     private SpriteRenderer spriteRenderer;
     public Sprite[] backGroundspriteList;
     void Start()
@@ -12,5 +14,6 @@ public class SpriteRandomaizer : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         int i = Random.Range(0, backGroundspriteList.Length);
         spriteRenderer.sprite = backGroundspriteList[i];
+        m_MinimapBG.sprite = backGroundspriteList[i];
     }
 }
